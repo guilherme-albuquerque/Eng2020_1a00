5937f1847f6fb5a00c925ba6cb37090fef476af6
Guilherme Rodrigues <guilherme.rodrigues@yaman.com.br>
2020-03-12 09:01:28 -0300

    criação de getter

--- src/Aluno.java
+++ src/Aluno.java
@@ -3,8 +3,13 @@ public class Aluno{
 	
    private String nome;
    private int id;
-   
-   
-   
+
+   public String getNome() {
+      return nome;
+   }
+
+   public int getId() {
+      return id;
+   }
 }
    
